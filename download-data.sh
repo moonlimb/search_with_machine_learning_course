65156520271dae267ccd1107c3b8bf12a1dfbd3f
Moon Limb <moonlimb@gmail.com>
2022-02-20 16:44:08 +0000

    Update index

--- download-data.sh
+++ download-data.sh
@@ -2,6 +2,7 @@ cd /workspace/datasets
 # TODO: put in validation checks
 #pip install kaggle
 echo "Downloading Kaggle"
+cd data
 kaggle competitions download -c acm-sf-chapter-hackathon-big
 unzip acm-sf-chapter-hackathon-big.zip
 tar -xf product_data.tar.gz
--- index-data.sh
+++ index-data.sh
@@ -3,6 +3,12 @@ usage()
   echo "Usage: $0 [-p /path/to/bbuy/products/field/mappings] [ -q /path/to/bbuy/queries/field/mappings ] [ -b /path/to/bbuy/products/logstash/conf ] [ -e /path/to/bbuy/queries/logstash/conf ] [ -l /path/to/logstash/home ] [ -g /path/to/write/logs/to ]"
   exit 2
 }
+echo "Killing previous logstash running instance"
+kill -i $(pgrep -f 'logstash')
+
+# echo "Delete indices"
+# delete indices in console
+# curl -k -x DELETE -u admin "https://localhost:9200/bb*" -H "Content-Type: application/json"
 
 PRODUCTS_JSON_FILE="/workspace/search_with_machine_learning_course/opensearch/bbuy_products.json"
 QUERIES_JSON_FILE="/workspace/search_with_machine_learning_course/opensearch/bbuy_queries.json"
@@ -32,10 +38,9 @@ shift $((OPTIND -1))
 
 echo "Creating index settings and mappings"
 echo " Product file: $PRODUCTS_JSON_FILE"
-echo " Query file: $QUERIES_JSON_FILE"
-curl -k -X PUT -u admin  "https://localhost:9200/bbuy_products" -H 'Content-Type: application/json' -d "@$PRODUCTS_JSON_FILE"
-echo ""
-curl -k -X PUT -u admin  "https://localhost:9200/bbuy_queries" -H 'Content-Type: application/json' -d "@$QUERIES_JSON_FILE"
+# echo " Query file: $QUERIES_JSON_FILE"
+curl -k -X PUT -u admin  "https://localhost:9200/bbuy_products_retry" -H 'Content-Type: application/json' -d "@$PRODUCTS_JSON_FILE"
+# curl -k -X PUT -u admin  "https://localhost:9200/bbuy_queries" -H 'Content-Type: application/json' -d "@$QUERIES_JSON_FILE"
 
 echo ""
 echo "Writing logs to $LOGS_DIR"
@@ -43,7 +48,7 @@ mkdir -p $LOGS_DIR
 
 echo "Indexing"
 echo " Product Logstash file: $PRODUCTS_LOGSTASH_FILE"
-echo " Query Logstash file: $QUERIES_LOGSTASH_FILE"
+# echo " Query Logstash file: $QUERIES_LOGSTASH_FILE"
 
 echo "Running Logstash found in $LOGSTASH_HOME"
 cd "$LOGSTASH_HOME"
@@ -51,6 +56,6 @@ echo "Launching Logstash indexing in the background via nohup.  See product_inde
 echo " Cleaning up any old indexing information by deleting products_data.  If this is the first time you are running this, you might see an error."
 rm -rf "$LOGSTASH_HOME/products_data"
 nohup bin/logstash --pipeline.workers 1 --path.data ./products_data -f "$PRODUCTS_LOGSTASH_FILE" > "$LOGS_DIR/product_indexing.log" &
-echo " Cleaning up any old indexing information by deleting query_data.  If this is the first time you are running this, you might see an error."
-rm -rf "$LOGSTASH_HOME/query_data"
-nohup bin/logstash --pipeline.workers 1 --path.data ./query_data -f "$QUERIES_LOGSTASH_FILE" > "$LOGS_DIR/queries_indexing.log" &
+# echo " Cleaning up any old indexing information by deleting query_data.  If this is the first time you are running this, you might see an error."
+# rm -rf "$LOGSTASH_HOME/query_data"
+# nohup bin/logstash --pipeline.workers 1 --path.data ./query_data -f "$QUERIES_LOGSTASH_FILE" > "$LOGS_DIR/queries_indexing.log" &
