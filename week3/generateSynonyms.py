b9cbd4c7c7c77076d7740cc203e0ca3baf4bae04
Moon Limb <moonlimb@gmail.com>
2022-03-07 14:39:14 +0000

    Get synonyms for test tokens

---
+++ week3/generateSynonyms.py
@@ -0,0 +1,80 @@
+from typing import List, Any
+
+import fasttext
+
+
+test_tokens = [
+    "gift", "camera", "laptop", "battery", "dishwasher", # products,
+    "apple", "sony", "barnes & noble", "microsoft", "bosch", # brands,
+    "2001", "beats by dr.dre", "playstation2", "ipad2", "integra 500", # models,
+    "silver", "pink", "1080p", "lcd", "100$", # attributes 
+]
+
+def train_model(input_file: str, min_count: int):
+    """Trains fasttext model with given configs, and returns model"""
+    # other ws, minn, maxn
+    kwargs = {
+        "input": input_file, # training data
+        "epoch": 50,
+        "minn": 0,
+        "maxn": 0,
+        "dim": 150,
+        "minCount": min_count,
+        "model": "skipgram"
+    }
+    model = fasttext.train_unsupervised(**kwargs)
+    return model
+
+
+def get_synonyms(model: Any, test_tokens: List[str]):
+    for token in test_tokens:
+        print(f"Test word: {token}")
+        neighbors = model.get_nearest_neighbors(token, 10)
+        for neighbor in neighbors[0:3]:
+            # print 3 nearest neighbors
+            print(neighbor)
+        
+if __name__ == "__main__":
+    # base_dir = '/workspace/datasets/fasttext/'
+    output_dir = "/workspace/datasets/fasttext/title_model"
+    model = None
+    model_exists = True
+
+    if not model_exists:
+        model = train_model(input_file="/workspace/datasets/fasttext/titles.txt", min_count=20)
+
+    model = fasttext.load_model(f"{output_dir}.bin")
+
+    model.save_model(output_dir)
+    get_synonyms(model, test_tokens)
+
+
+
+#### Results
+'''
+
+
+'''
+# gift card: gift => buy (0.8),
+# camera,
+# laptop,
+# battery => batteri, lithiumion (0.65)
+# dishwasher
+# brand:
+# best buy,
+# apple => apple®, appl
+# sony -> soni, sonic
+# barnes & nobles => nobl, barn
+# microsoft
+# model:
+# 2001,
+# beats by dr.dre,
+# playstation 2 => ps3
+# integra 500 (bosch) => intel, intern, internet :thinking_face:
+# ipad2 => ipad, ipad®, ipad™
+# attributes:
+# silver => silverblack
+# 100$ => 100w, 1000w
+# lcd => ledlcd, led
+# 1080p -> hdtv, 240hz
+# pink => ink, green, red…. :thinking_face:
